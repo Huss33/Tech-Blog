93e84017459dfe090eb79858c135d3acdd6c64ef
Bear Ridge <ahuss2010@live.com>
2022-02-04 00:20:16 -0600

    finished setup, first route possibly complete

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,12 @@
+const router = require('express').Router();
 
+const apiRoutes = require('./api');
+// const homeRoutes = require('./home-routes');
+// const dashRoutes = require('./dashboard-routes');
+
+router.use('/api', apiRoutes);
+// router.use('/home-routes', homeRoutes);
+// router.use('/dash', dashRoutes);
+
+
+module.exports = router;
